ff63fe3c91a679836b22671be198ef8e1e8e7e66
prathishpratt <techwizprathish@gmail.com>
2023-08-25 19:44:20 +0530

    easy

--- InterviewBit/empty neighbourhoods.sql
+++ InterviewBit/empty neighbourhoods.sql
@@ -1,5 +1,12 @@
-select d.name from neighborhoods as d
-left join users u
-on u.neighborhood_id = d.id
-group by d.id
-having count(u.id) = 0 
+select n.name
+from neighborhoods as n
+left join users as u
+on n.id = u.neighborhood_id
+where u.id is null
+
+--This also works
+-- select d.name from neighborhoods as d
+-- left join users u
+-- on u.neighborhood_id = d.id
+-- group by d.id
+-- having count(u.id) = 0 
