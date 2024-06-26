b466428f02f0a4d697fe733e68b67536794d2bea
Prathish <64516584+prathishpratt@users.noreply.github.com>
2024-06-26 09:38:00 +0530

    Valid Palindrome

--- 125.py
+++ 125.py
@@ -1,7 +1,25 @@
-def isPalindrome(self, s: str) -> bool: 
-        k= ""
-        for i in s:
-            if i.isalnum():
-                k=k+i
-        k=k.lower()
-        return k==k[::-1]
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #EXTRA MEMORY
+        # k = []
+        # for i in s:
+        #     if i.isalnum():
+        #         k = k+[i.lower()]
+        # return k == k[::-1]
+        
+        l = 0
+        r = len(s)-1
+
+        #2 POINTER
+        while(r>=l):
+            if s[l].isalnum() and s[r].isalnum():
+                if s[l].lower() == s[r].lower():
+                    l += 1
+                    r -= 1
+                else:
+                    return False
+            elif s[l].isalnum():
+                r -=1
+            else:
+                l +=1
+        return True
