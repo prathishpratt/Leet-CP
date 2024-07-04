8b9dd46041d1f790b8e105d151534c79bf4a9ac1
Prathish <64516584+prathishpratt@users.noreply.github.com>
2024-07-05 05:21:13 +0530

    Longest Repeating Character Replacement

--- 424.py
+++ 424.py
@@ -1,13 +1,14 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count = {}
-        l = 0
+        l, r = 0, 0
+        has = {}
         res=0
 
-        for r in range(0,len(s)):
-            count[s[r]] = 1 + count.get(s[r],0)
-            if ((r-l+1) - max(count.values()) > k):
-                count[s[l]] -=1
-                l = l+1
-            res= max(res,r-l+1)
+        while(r<len(s)):
+            has[s[r]] = 1+ has.get(s[r],0)
+            if (r-l+1)-max(has.values()) >k:
+                has[s[l]] -= 1
+                l +=1
+            res = max(res,r-l+1)
+            r +=1
         return res
