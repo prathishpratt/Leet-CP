066511fff4edba1d74377c05ae8a5e7552566daa
Prathish <64516584+prathishpratt@users.noreply.github.com>
2024-07-17 03:20:09 +0530

    Find the Duplicate Number

--- 287.py
+++ 287.py
@@ -1,17 +1,17 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        #Using flyod's algorithm
-        slow,fast = 0,0
+        slow = 0
+        fast = 0
 
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
-            if slow == fast:
+            if fast == slow:
                 break
         slow = 0
         while True:
-            fast = nums[fast]
             slow = nums[slow]
+            fast = nums[fast]
             if slow == fast:
                 return slow
 
