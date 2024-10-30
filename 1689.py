23a245b2adbe97d62eda62d7f90b084a0563e745
Prathish <64516584+prathishpratt@users.noreply.github.com>
2024-10-30 09:00:22 +0530

    Partitioning Into Minimum Number Of Deci-Binary Numbers

---
+++ 1689.py
@@ -0,0 +1,5 @@
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        # Logic is to return the largest digit in the string
+        # return int(max(list(n)))
+        return int(max(n))
