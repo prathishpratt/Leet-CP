ecaf0eebc274f07ba887f0caadee7f6d471c07db
Prathish <64516584+prathishpratt@users.noreply.github.com>
2024-11-05 13:30:47 +0530

    Happy Number

--- 202.py
+++ 202.py
@@ -1,7 +1,7 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         for i in range(20):
-            n= sum([int(i)*int(i)   for i in str(n)])
+            n= sum([int(i)*int(i)  for i in str(n)])
             if n==1:
                 return True
         return False
