38c7abcf3ea6ee808f53c6454a7dd17c27931189
Prathish Murugan <64516584+prathishpratt@users.noreply.github.com>
2025-02-02 04:08:35 +0530

    Valid Parentheses

--- 20.py
+++ 20.py
@@ -1,31 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # i = 0
-        # j = len(s)-1
-
-        # while(j>i):
-        #     if s[i] == "(":
-        #         if s[j] != ")":
-        #             return False
-        #     elif s[i] == "{":
-        #         if s[j] != "}":
-        #             return False
-        #     elif s[i] == "[":
-        #         if s[j] != "]":
-        #             return False
-        #     i = i+1
-        #     j = j-1
-        # return True
-        
-        openclose = {']':'[', '}':'{', ')':'('}
         stack = []
-
+        valid = { ")" : "(", "]" : "[", "}" : "{" }
+        
         for i in range(0,len(s)):
-            if s[i] in openclose.keys() and stack:
-                if stack[-1] == openclose[s[i]]:
+            if s[i] in valid.keys():
+                if stack and stack[-1] == valid[s[i]]:
                     stack.pop()
                 else:
                     return False
             else:
                 stack.append(s[i])
-        return False if stack else True
+        if stack:
+            return False
+        else:
+            return True
